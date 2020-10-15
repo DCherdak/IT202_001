dafbb93ef866e360d116c1d1363bc289fc346c84
DCherdak <56012603+DCherdak@users.noreply.github.com>
2020-10-15 13:36:18 -0400

    Update db.php

--- dbtest/db.php
+++ dbtest/db.php
@@ -1,28 +1,30 @@
+
 <?php
-//include our db file
-require("db.php");
-//use the function that we just pulled in
-$db = getDB();
-//make sure it's set
-if(isset($db)){
-        //we'll fetch our sql file from earlier
-        $query = file_get_contents("create_table_users.sql");
-        //prepares the query safely to reduce SQL injection (discussed later)
-        $stmt = $db->prepare($query);
-        //runs the query
-        $stmt->execute();
-        //checks if we have an error info populated
-        //by default it's always populated so success is if index 0 is 5 zeroes
-        $e = $stmt->errorInfo();
-        if($e[0] != '00000'){
-                echo "Query error: " . var_export($e, true);       
+//for this we'll turn on error output so we can try to see any problems on the screen
+//this will be active for any script that includes/requires this one
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+function getDB(){
+    global $db;
+    //this function returns an existing connection or creates a new one if needed
+    //and assigns it to the $db variable
+    if(!isset($db)) {
+        try{
+            //__DIR__ helps get the correct path regardless of where the file is being called from
+            //it gets the absolute path to this file, then we append the relative url (so up a directory and inside lib)
+            require_once(__DIR__. "/../lib/config.php");//pull in our credentials
+            //use the variables from config to populate our connection
+            $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+            //using the PDO connector create a new connect to the DB
+            //if no error occurs we're connected
+            $db = new PDO($connection_string, $dbuser, $dbpass);
         }
-        else{
-                echo "table created successfully";
+    catch(Exception $e){
+            var_export($e);
+            $db = null;
         }
-        
-}
-else{
-        echo "there may be a problem with our connection details";
+    }
+    return $db;
 }
 ?>
